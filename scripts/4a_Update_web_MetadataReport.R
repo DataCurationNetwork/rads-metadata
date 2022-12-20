2ea7e2f29f549e8a375118a1e4539280494f0c47
Alicia Hofelich Mohr <hofelich@umn.edu>
2022-12-19 22:08:12 -0600

    updated to correct file path

--- scripts/4a_Update_web_MetadataReport.R
+++ scripts/4a_Update_web_MetadataReport.R
@@ -1,3 +1,3 @@
 ## Update html for RMD paper file
 
-rmarkdown::render('scripts/4_MetadataAnalysis_forpaper.Rmd', output_file = 'docs/Metadata_report.html', envir = new.env())
+rmarkdown::render('scripts/4_MetadataAnalysis_forpaper.Rmd', output_file = '../docs/Metadata_report.html', envir = new.env())
